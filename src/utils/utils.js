ee4da577629f7cc311c6376f8a677d281db83ff2
sanjayc <sanjayc@sanjayc.(none)>
2013-01-22 18:33:45 +0530

    Added the property to generate tree view from ChartXML

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,5 +1,6 @@
 fc = {};
 fc.utils = {
+	
 	addProperty : function (node, key, value) {
 		if (value !== undefined) {
 			node.attributes[key] = value;
@@ -14,7 +15,7 @@ fc.utils = {
 			propertyString += " " + key + "='" + value + "'";
 		});
 		
-		return propertyString;
+		return propertyString;l
 	},
 
 	extend : function (f) {
@@ -29,13 +30,78 @@ fc.utils = {
 
 	getPropertyMap : function (propertyString) {
 		var propertyMap = {};
-		$(propertyString).each(function () {
+		var commonPropertyMap = {};
+		var specificProperties = {};
+		$(propertyString).each(function (index) {
+			fc.utils.initializeIfUndefined(commonPropertyMap, this.tagName, 'Object');  
+			fc.utils.initializeIfUndefined(specificProperties, this.tagName, 'Object'); 
+
 			var tagName = this.tagName;
-			propertyMap[tagName] = {};
+			fc.utils.initializeIfUndefined(propertyMap, tagName, 'Object');
+			fc.utils.initializeIfUndefined(propertyMap[tagName], '_variable_', 'Object');
+			fc.utils.initializeIfUndefined(propertyMap[tagName], '_defaults_', 'Object');
+
 			$.each(this.attributes, function (i, attrib) {
-				propertyMap[tagName][attrib.name] = attrib.value;
+				if (index === 0) {
+					commonPropertyMap[tagName][attrib.name] = attrib.value;
+				} else if(commonPropertyMap[tagName][attrib.name] === undefined || commonPropertyMap[tagName][attrib.name] !== attrib.value  ) {
+					delete commonPropertyMap[tagName][attrib.name];
+					specificProperties[tagName][attrib.name] = null;
+				}				
 			});
+			propertyMap[tagName]['_variable_'] = specificProperties[tagName];
+			propertyMap[tagName]['_defaults_'] = commonPropertyMap[tagName];
+			propertyMap[tagName]['_children_'] = fc.utils.getPropertyMap(this.children);
+			
 		});
 		return propertyMap;
+	},
+
+	initializeIfUndefined : function (map,key,type) {
+		if(map[key] === undefined) {
+			if(type === 'Object') {
+				map[key] = {};
+			}
+			else if (type === 'Array') {
+				map[key] = [];
+			}
+		}
+	},
+
+	getTreeView : function(propertyMap) {
+
+		if ($.type(propertyMap) === 'array') {
+			var returnString = '<li>';
+			$.each(propertyMap, function(idx, data) {
+				returnString += '<ul>';
+				$.each(data, function (key, value) {
+					returnString += '<li>' + key + ' : ' + value + '</li>';
+				});
+				returnString += '</ul>';			
+			});
+			return returnString + '</li>';
+		} 
+		else if ($.type(propertyMap) === 'object') {
+			var htmlString = "";
+			$.each(propertyMap , function (key, data){
+				htmlString += '<ul>';
+				
+				if($.type(data) === 'array') {
+					htmlString += fc.utils.getTreeView(data);	
+				} else {
+					htmlString += '<li>' + key + ' : ' + fc.utils.getTreeView(data) +'</li>';	
+				}
+			
+				htmlString +='</ul>';
+			});	
+		} 
+		else if ($.type(propertyMap) === 'string') {
+			return propertyMap;
+		} else {
+			console.log(propertyMap + ' : Unsupported type passed as argument.');
+		}
+		
+
+		return htmlString;
 	}
 };
